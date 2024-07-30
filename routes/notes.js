2a7d4666d36878502632d0b49185c036d9845b5c
taikiatsu102 <ribonny3@gmail.com>
2024-07-31 08:09:18 +0900

    mongodb

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,31 @@
 var express = require('express');
 var router = express.Router();
+
+
+// 接続情報を設定
+const { MongoClient } = require("mongodb");
+const uri = "mongodb+srv://ribonny3:admiN000@cluster0.tqjzrpg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const client = new MongoClient(uri);
+router.get('/', async (req, res) => {
+    // データベース、コレクションを指定
+    const database = client.db('notes');
+    const notes = database.collection('notes');
+    
+    // idが１のドキュメントを取得
+    const query = { id: 2 };
+    const note = await notes.findOne(query);
+    res.json(note);
+})
+
+module.exports = router;
+
+
+
+
+
+
 // レスポンスのデータ（ノート全件）
+/*
 const responseObjectDataAll = {
  textObject1 : {
  id: 1,
@@ -15,6 +40,7 @@ const responseObjectDataAll = {
  bodyText: 'ノート２の本文です'
  },
  };
+ */
  /**
  * メモを全件取得するAPI
  * @returns {Object[]} data
@@ -22,8 +48,10 @@ const responseObjectDataAll = {
  * @returns {string} data.title - タイトル
 * @returns {string} data.text - 内容
 */
+/*
  router.get('/', function (req, res, next) {
  // 全件取得して返す
 res.json(responseObjectDataAll);
  })
  module.exports = router;
+ */
